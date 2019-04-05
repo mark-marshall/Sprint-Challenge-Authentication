406357c89b5b7e66322f6450461da89ff3bfb412
mark-marshall <mark.marshallgp@gmail.com>
2019-04-05 08:24:18 +0100

    [MVP] Completed register functionality with token gen

--- config/routes.js
+++ config/routes.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 const bcrypt = require('bcryptjs');
 
 const { authenticate } = require('../auth/authenticate');
+const db = require('../database/dbConfig');
 const generateToken = require('../auth/generateToken');
 
 module.exports = server => {
@@ -11,7 +12,24 @@ module.exports = server => {
 };
 
 function register(req, res) {
-  // implement user registration
+user = req.body;
+if(user.username && user.password) {
+  const { password } = req.body;
+  // Create hashed pw using bcrypt and multiple hashing rounds
+  const hashedPw = bcrypt.hashSync(password, 10);
+  user.password = hashedPw;
+  // Add user with hashed pw to the database
+  db('users')
+  .insert(user)
+  .then(id => {
+    // Create new token for user so they can be auto-logged in on registration
+    const token = generateToken(user);
+    const { username } = user;
+    res.status(201).json({ message: `welcome in ${username}`, token })
+  })
+} else {
+  res.status(400).json({ message: 'please enter a username and password' })
+}
 }
 
 function login(req, res) {
