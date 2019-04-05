bfc16a61ee37c5df146bfddd6569b1b45c924a78
mark-marshall <mark.marshallgp@gmail.com>
2019-04-05 09:25:12 +0100

    [STRETCH] Added async requests to server endpoints

--- client/src/Components/Container.jsx
+++ client/src/Components/Container.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import axios from '../axios/axios';
+import * as url from '../consts/urlConsts';
 
 class Container extends Component {
   state = {
@@ -8,6 +9,20 @@ class Container extends Component {
       username: '',
       password: '',
     },
+    jokes: [],
+    error: '',
+  };
+
+  setError = error => {
+    this.setState({
+      error,
+    });
+  };
+
+  setJokes = jokes => {
+    this.setState({
+      jokes,
+    });
   };
 
   resetUser = () => {
@@ -34,6 +49,65 @@ class Container extends Component {
 
   clearItemLocalStore = key => {
     localStorage.removeItem(key);
+  };
+
+  replaceUrl = path => {
+    this.props.history.replace(path);
+  };
+
+  fireSignUp = user => {
+    this.signUpAsync(user);
+    this.resetUser();
+  };
+
+  signUpAsync = user => {
+    axios()
+      .post(url.signUp, user)
+      .then(res => {
+        const newToken = res.data.token;
+        this.setItemLocalStore('token', newToken);
+        this.replaceUrl('/users');
+      })
+      .catch(error => {
+        const newError = error.message;
+        this.setError(newError);
+      });
+  };
+
+  fireSignIn = user => {
+    this.signInAsync(user);
+    this.resetUser();
+  };
+
+  signInAsync = user => {
+    axios()
+      .post(url.signIn, user)
+      .then(res => {
+        const newToken = res.data.token;
+        this.setItemLocalStore('token', newToken);
+        this.replaceUrl('/users');
+      })
+      .catch(error => {
+        const newError = error.message;
+        this.setError(newError);
+      });
+  };
+
+  grabJokesAsync = () => {
+    axios()
+      .get(url.jokes)
+      .then(jokes => {
+        if (jokes.length > 0) {
+          const newJokes = jokes.data;
+          this.setJokes(newJokes);
+        } else {
+          this.setError('we have no jokes');
+        }
+      })
+      .catch(error => {
+        const newError = error.message;
+        this.setError(newError);
+      });
   };
 
   render() {
